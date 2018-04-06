f86ba30fa07b628d306a537993e47393c06eac4b
Akito Ito <akito0107@gmail.com>
2018-04-06 15:49:30 +0900

    feat: random gen

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const assert = require("assert");
 const logger = require("pino")();
+const RandExp = require("randexp");
 
 async function main(scenario) {
   const browser = await puppeteer.launch();
@@ -12,34 +13,13 @@ async function main(scenario) {
   logger.info("precondition start.");
   const precondition = scenario["precondition"];
   await goto(page, precondition["url"]);
-  await run(precondition["steps"]);
-  await ensure(page, precondition["ensure"]);
+  await run(page, precondition["steps"]);
   logger.info("precondition done.");
-
   logger.info("main scenario start.");
   await goto(page, scenario["url"]);
+  await run(page, scenario["steps"]);
 
   await browser.close();
-}
-
-async function type(page, name, value) {
-  console.log(name, value);
-  try {
-    await page.type(`input[name="${name}"]`, value);
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
-
-async function select(page, name, value) {
-  console.log(name, value);
-  try {
-    await page.select(`select[name="${name}"]`, value);
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
 }
 
 async function goto(page, url) {
@@ -51,8 +31,18 @@ async function run(page, steps) {
     const action = step["action"];
     switch (action["type"]) {
       case "input":
-        const form = action["form"];
-        await page.type(form["selector"], form["value"]);
+        const input = action["form"];
+        if (input["value"]) {
+          await page.type(input["selector"], input["value"]);
+        } else {
+          const regex = new RegExp(input["constrains"]["regexp"])
+
+          const randex = new RandExp(regex);
+          randex.defaultRange.subtract(32, 126);
+          randex.defaultRange.add(0, 65535);
+
+          await page.type(input["selector"], randex.gen());
+        }
         break;
       case "wait":
         await page.waitFor(action["duration"]);
@@ -61,8 +51,19 @@ async function run(page, steps) {
         const target = await page.$(action["selector"]);
         await target.click();
         break;
+      case "select":
+        const select = action["form"];
+        const v = select["constrains"]["values"];
+        await page.select(
+          select["selector"],
+          `${v[Math.floor(Math.random() * v.length)]}`
+        );
+        break;
+      case "ensure":
+        await ensure(page, action);
+        break;
       default:
-        throw new Error("unknown action type");
+        throw new Error(`unknown action type: ${action["type"]}`);
     }
   }
 }
@@ -70,7 +71,26 @@ async function run(page, steps) {
 async function ensure(page, conds) {
   if (conds["location"]) {
     const url = await page.url();
-    assert.strictEqual(url, ensure["location"]);
+
+    if (conds["location"]["value"]) {
+      assert.strictEqual(
+        url,
+        conds["location"]["value"],
+        `location check failed: must be ${
+          conds["location"]["value"]
+        }, but: ${url}`
+      );
+    }
+
+    if (conds["location"]["regexp"]) {
+      const regexp = new RegExp(conds["location"]["regexp"]);
+      assert(
+        regexp.test(url),
+        `location check failed: must be ${
+          conds["location"]["regexp"]
+        }, but: ${url}`
+      );
+    }
   }
 }
 
